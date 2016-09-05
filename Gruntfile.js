163522805dd2dd4de5b75efd9225cc8f1313867a
黃漢庭 <huhantin@hantinteki-MacBook-Pro.local>
2016-09-05 12:05:49 +0800

    update 0905  刪除css

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,20 +3,80 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
 
-      //cssmin
+        //watch
+        watch: {
+            css: {
+                files: ['assets/sass/*.scss', 'assets/sass/**/*.scss'],
+                tasks: ['sass']
+            },
+            styles: {
+                files: ['assets/css/mini/minicss.css'],
+                tasks: ['cssmin']
+            },
+            html: {
+                files: ['*.html']
+            },
+            js: {
+                files: ['assets/js/*.js']
+            }
+        },
+        //sass
+        sass: {
+            build: {
+                options: {
+                    sourcemap: 'none'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'assets/sass/views/',
+                    src: ['*.scss'],
+                    dest: 'assets/css/',
+                    ext: '.css'
+                }]
+            }
+        },
+        //cssmin
         cssmin: {
             target: {
                 files: {
-                    'minicss.css': ['foo.css', 'bar.css']
+                    'assets/css/mini/minicss.css': ['assets/css/*.css']
                 }
             }
         },
-
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: [
+                        'assets/css/*.css',
+                        '*.html',
+                        'assets/css/**/*.css',
+                        'assets/js/*.js'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: './',
+                        index: 'index.html'
+                    }
+                }
+            }
+        },
     });
-
-    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    // grunt.loadNpmTasks('grunt-contrib-clean');
+    // grunt.loadNpmTasks('grunt-contrib-jade');
+    // grunt.loadNpmTasks('grunt-contrib-jshint');
+    // grunt.loadNpmTasks('grunt-contrib-concat');
+    // grunt.loadNpmTasks('grunt-browserify');
+    // grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    // Default task(s).
-    grunt.registerTask('default', ['cssmin']);
+
+    // define default task
+    // grunt.registerTask('default', ['browserSync','browserify', 'watch', 'jade', 'jshint']);
+    grunt.registerTask('default', ['browserSync', 'watch', 'cssmin']);
+    grunt.registerTask('mini', ['cssmin']);
 
 };
