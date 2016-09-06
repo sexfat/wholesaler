93bff129f5a76821bdfb7b1a7eaf98b1fba788dd
黃漢庭 <huhantin@hantinteki-MacBook-Pro.local>
2016-09-06 16:36:48 +0800

    update 0906

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,21 +62,55 @@ module.exports = function(grunt) {
                 }
             }
         },
+
+     //打包用
+        compress: {
+            main: {
+                options: {
+                    archive: function() {
+                        return 'online/' + 'project' + (new Date()).getDate() + (new Date()).getHours() + (new Date()).getMinutes() + '.zip'
+                    }
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'assets/css/mini',
+                    src: ['*.css'],
+                    dest: 'assets/css/'
+                }, {
+                    expand: true,
+                    cwd: 'assets/img/',
+                    src: ['*'],
+                    dest: 'assets/img/'
+                }, {
+                    expand: true,
+                    cwd: './',
+                    src: ['*.html'],
+                    dest: '/'
+                }, {
+                    expand: true,
+                    cwd: 'assets/js/',
+                    src: ['*.js'],
+                    dest: 'assets/js/'
+                }]
+            }
+        },
+
     });
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-browser-sync');
-    // grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-clean');
     // grunt.loadNpmTasks('grunt-contrib-jade');
     // grunt.loadNpmTasks('grunt-contrib-jshint');
     // grunt.loadNpmTasks('grunt-contrib-concat');
     // grunt.loadNpmTasks('grunt-browserify');
-    // grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // define default task
     // grunt.registerTask('default', ['browserSync','browserify', 'watch', 'jade', 'jshint']);
-    grunt.registerTask('default', ['browserSync', 'watch', 'cssmin']);
+    grunt.registerTask('default', ['browserSync', 'watch']);
     grunt.registerTask('mini', ['cssmin']);
+    grunt.registerTask('build', ['compress']);
 
 };
